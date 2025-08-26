1e77ec53fd2fe6db86bd563e41fc44dc7dd68586
abergman <abergman@student.42.fr>
2025-08-26 13:16:01 +0000

    feat(*): added index files

---
+++ webpack-configuration/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Salut à tous</h3>
+            </div>
+        )
+    }
+}
+
+export default App;
