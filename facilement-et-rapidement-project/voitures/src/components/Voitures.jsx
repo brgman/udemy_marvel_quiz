1319581b60b10d755823cac19592fef044500923
abergman <abergman@student.42.fr>
2025-08-27 14:30:25 +0000

    feat(*): added class Voitures

---
+++ facilement-et-rapidement-project/voitures/src/components/Voitures.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Voitures extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello world !</h2>
+                <p>Ceci la text</p>
+            </div>
+        );
+    }
+}
+
+export default Voitures;
